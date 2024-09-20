6f6bc4f8e56ce4e035d53e9f49d34371f2404d4b
oznchex <114240463+oznchex@users.noreply.github.com>
2024-09-20 15:46:59 +0900

    [feat]: Update action.ts API

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -11,7 +11,7 @@ export async function GetRecommendProjects(accessToken: string) {
     },
     credentials: 'include',
   })
-  return response;
+  return await response.json();
 }
 
 // 1.2. (access-token 만료 시) 토큰 재발급 (POST /token) 
@@ -24,8 +24,7 @@ export const RefreshAccessToken = async (accessToken: string) => {
     },
     credentials: 'include',
   })
-  const data = await response.json();
-  return data
+  return await response.json();
 }
 
 // 로그아웃
@@ -39,7 +38,7 @@ export async function Logout(accessToken: string) {
     credentials: 'include',
   })
 
-  return response
+  return await response.json();
 }
 
 // 회원탈퇴
@@ -53,7 +52,7 @@ export async function Withdrawal(accessToken: string) {
     credentials: 'include',
   })
 
-  return response
+  return await response.json();
 }
 
 // 2.1. 채팅방 목록 조회 (GET /chat)
@@ -73,8 +72,6 @@ export async function GetChatList(accessToken: string) {
   return response.json(); // 응답 데이터를 JSON 형식으로 반환
 }
 
-// 2.2. 
-
 // 2.4. 새로운 채팅방 생성 (POST /chat/v2)
 export async function GetChatLocation(accessToken: string): Promise<string | null> {
   const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/chat/v2`, {
@@ -91,7 +88,7 @@ export async function GetChatLocation(accessToken: string): Promise<string | nul
     return response.headers.get('Location');
   }
 
-  return null;
+  return await response.json();
 }
 
 
@@ -107,7 +104,7 @@ export async function SendMessage(accessToken: string, chatRoomId: string, messa
     body: JSON.stringify({ message }),
   })
 
-  return response
+  return await response.json();
 }
 
 // 2.6. 요약 노트 생성
@@ -120,7 +117,7 @@ export async function CreateSummaryNote(accessToken: string, chatRoomId: string)
     credentials: 'include',
   })
 
-  return response
+  return await response.json();
 }
 
 // 2.7. 채팅방 삭제
@@ -133,7 +130,7 @@ export async function DeleteChatRoom(accessToken: string, chatRoomId: string) {
     credentials: 'include',
   })
 
-  return response
+  return await response.json();
 }
 
 // 2.8. 채팅 내역 조회 ( GET /chat/:chatRoomId/history )
@@ -146,7 +143,7 @@ export async function GetChatHistory(accessToken: string, chatRoomId: string) {
     credentials: 'include',
   })
 
-  return response
+  return await response.json();
 }
 
 // 3.1. 요약 노트 목록 조회 (GET /note)
@@ -163,7 +160,7 @@ export async function GetSummaryNoteList(accessToken: string) {
     throw new Error('Failed to fetch chat list');
   }
 
-  return response.json();
+  return await response.json();
 }
 
 // 3.2. 요약 노트 상세 조회 (GET /note/:noteId)
@@ -175,8 +172,7 @@ export async function GetSummaryNoteContent(accessToken: string, noteId: string)
     },
     credentials: 'include',
   })
-
-  return response
+  return await response.json();
 }
 
 // 3.3. 요약 노트 삭제 (DELETE /note/:noteId)
@@ -189,7 +185,20 @@ export async function DeleteSummaryNote(accessToken: string, noteId: string) {
     credentials: 'include',
   })
 
-  return response
+  return await response.json();
+}
+
+// 3.4. 기획서 자동 완성하기 (GET /note/{noteId}/completion)
+export async function CompleteSummaryNote(accessToken: string, noteId: string) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/note/${noteId}/completion`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: 'include',
+  })
+
+  return await response.json();
 }
 
 // 4.1. 프로젝트 개요 저장 (POST /project/outline/save)
@@ -209,10 +218,43 @@ export async function PostProjectOutlineData(accessToken: string, payload: any, 
     credentials: 'include',
     body: formData,
   })
-  return response
+  return await response.json();
 }
 
-// 3.4. 프로젝트 본문 저장 (POST /project/{projectId}/body/save)
+// 4.2. 프로젝트 개요 조회 (GET /project/{projectId}/outline)
+export async function GetProjectOutlineData(accessToken: string, projectId: number) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/project/${projectId}/outline`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: 'include',
+  })
+
+  return await response.json();
+}
+
+// 4.3. 프로젝트 개요 수정 (POST /project/{projectId}/outline/edit)
+export async function EditProjectOutlineData(accessToken: string, projectId: number, payload: any, projectRepresentImage: File | null) {
+  const formData = new FormData()
+  formData.append('dto', new Blob([JSON.stringify(payload)], { type: 'application/json' }))
+
+  if (projectRepresentImage) {
+    formData.append('file', projectRepresentImage)
+  }
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/project/${projectId}/outline/edit`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: 'include',
+    body: formData,
+  })
+  return await response.json();
+}
+
+// 4.4. 프로젝트 본문 저장 (POST /project/{projectId}/body/save)
 export async function PostProjectBodyData(accessToken: string, projectId: number, payload: any, files: File[]) {
   const formData = new FormData();
 
@@ -233,9 +275,71 @@ export async function PostProjectBodyData(accessToken: string, projectId: number
     body: formData, // FormData 객체를 body로 설정
   });
 
-  return response;
+  return await response.json();
 }
 
+// 4.5. 프로젝트 본문 조회 (GET /project/{projectId}/body)
+export async function GetProjectBodyData(accessToken: string, projectId: number) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/project/${projectId}/body`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: 'include',
+  })
+
+  return await response.json();
+}
+
+// 4.6. 프로젝트 본문 수정 (POST /project/{projectId}/outline/edit)
+export async function EditProjectBodyData(accessToken: string, projectId: number, payload: any, files: File[]) {
+  const formData = new FormData();
+
+  // JSON 데이터를 문자열로 변환하여 formData에 추가
+  formData.append('dto', new Blob([JSON.stringify(payload)], { type: 'application/json' }));
+
+  // 파일 데이터를 formData에 추가
+  files.forEach((file) => {
+    formData.append('files', file);
+  });
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/project/${projectId}/body/edit`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: 'include',
+    body: formData, // FormData 객체를 body로 설정
+  });
+
+  return await response.json();
+}
+
+// 4.7. 프로젝트 등록 (POST /project/{projectId}/register)
+export async function RegisterProject(accessToken: string, projectId: number) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/project/${projectId}/register`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: 'include',
+  })
+
+  return await response.json();
+}
+
+// 4.8. 프로젝트 삭제 (DELETE /project/{projectId})
+export async function DeleteProject(accessToken: string, projectId: number) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/project/${projectId}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: 'include',
+  })
+
+  return await response.json();
+}
 
 // 5.1. 프로젝트 갤러리 상세 조회 (GET /gallery/{projectId})
 export async function GetProjectGalleryDetail(accessToken: string, projectId: number) {
@@ -246,7 +350,43 @@ export async function GetProjectGalleryDetail(accessToken: string, projectId: nu
     },
     credentials: 'include',
   })
-  return response
+  return await response.json();
+}
+
+// 5.2. 프로젝트 갤러리 조회 (GET /gallery)
+export async function GetProjectGallery(accessToken: string) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/gallery`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: 'include',
+  })
+  return await response.json();
+}
+
+// 5.3. 프로젝트 갤러리 키워드로 검색 (GET /gallery/search/keyword?page=0&size=10&keyword=검색어)
+export async function SearchProjectGallery(accessToken: string, keyword: string) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/gallery/search/keyword?page=1&size=8&keyword=${keyword}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: 'include',
+  })
+  return await response.json();
+}
+
+// 5.4. 프로젝트 갤러리 태그로 검색 (GET /gallery/search/tag?page=0&size=10&tag=태그)
+export async function SearchProjectGalleryByTag(accessToken: string, tag: string) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/gallery/search/tag?page=1&size=8&tag=${tag}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: 'include',
+  })
+  return await response.json();
 }
 
 // 6.1. 프로젝트 좋아요 상태 변경 (POST /project/{projectId}/like)
@@ -259,7 +399,7 @@ export async function ToggleProjectLike(accessToken: string, projectId: number) 
     credentials: 'include',
   })
 
-  return response
+  return await response.json();
 }
 
 // 7.1. 마이페이지 조회 API (GET /mypage)
@@ -272,12 +412,11 @@ export async function GetMyPage(accessToken: string) {
     credentials: 'include',
   })
 
-  const data = await response.json()
-  return data
+  return await response.json();
 }
 
 
-// 7.2. 마이페이지 수정 API (POST /mypage/update)
+// 7.2. 프로필 수정 API (POST /mypage/update)
 export async function UpdateMyPage(accessToken: string, payload: any, profileImage: File | null) {
   const formData = new FormData()
   formData.append('dto', new Blob([JSON.stringify(payload)], { type: 'application/json' }))
@@ -295,5 +434,5 @@ export async function UpdateMyPage(accessToken: string, payload: any, profileIma
     body: formData,
   })
 
-  return response
+  return await response.json();
 }
