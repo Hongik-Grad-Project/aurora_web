8b0877cd3d898d95599b44af7264212e6dfe0b39
oznchex <114240463+oznchex@users.noreply.github.com>
2024-09-21 22:18:40 +0900

    [feat]: 응원완료 컴포넌트 구현

--- src/components/Project/CheerButton.tsx
+++ src/components/Project/CheerButton.tsx
@@ -12,19 +12,7 @@ export default function CheerButton({ isLiked, likeCount, onToggleLike }: CheerB
     return isLiked ? (
         <div className="flex flex-col items-start gap-3 w-[113px] shrink-0">
             <button
-                className="flex items-center justify-center w-[113px] h-[51px] rounded-full border bg-[#F4F6FA] text-gray-600 border-[#E2E6EF]"
-                onClick={onToggleLike}
-            >
-                응원하기 {likeCount}
-            </button>
-            <span>
-                응원하기 취소
-            </span>
-        </div>
-    ) : (
-        <div className="flex flex-col items-center gap-3 w-[113px] shrink-0">
-            <button
-                className="flex items-center justify-center w-[113px] h-[51px] rounded-full border bg-[#f4f6fa] text-black border-[#E2E6EF]"
+                className="flex items-center justify-center h-[51px] self-stretch rounded-[30px] border border-[#776BFF] bg-[#FEFEFE] text-gray-600"
                 onClick={onToggleLike}
             >
                 <Image
@@ -35,7 +23,30 @@ export default function CheerButton({ isLiked, likeCount, onToggleLike }: CheerB
                     className="mr-2" // Right margin to space icon and text
                 />
                 <span
-                    className="text-[#0F1011] font-pretendard font-bold text-[19px] leading-[28.5px]"
+                    className="flex w-[34px] h-[38px] flex-col justify-center text-[#776BFF] font-pretendard font-bold text-[19px] leading-[28.5px]"
+                >
+                    {likeCount}
+                </span>
+            </button>
+            <span className="w-full text-center text-[#776BFF] font-pretendard font-semibold text-[16px] leading-[24px]">
+                응원완료
+            </span>
+        </div>
+    ) : (
+        <div className="flex flex-col items-center gap-3 w-[113px] shrink-0">
+            <button
+                className="flex items-center justify-center h-[51px] self-stretch rounded-[30px] border bg-[#f4f6fa] text-black border-[#E2E6EF]"
+                onClick={onToggleLike}
+            >
+                <Image
+                    src="/assets/icons/cheer_button.svg"
+                    width={24}
+                    height={30}
+                    alt="Cheer icon"
+                    className="mr-2" // Right margin to space icon and text
+                />
+                <span
+                    className="flex w-[34px] h-[38px] flex-col justify-center text-[#0F1011] font-pretendard font-bold text-[19px] leading-[28.5px]"
                 >
                     {likeCount}
                 </span>
