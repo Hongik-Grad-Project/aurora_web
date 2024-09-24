803cea503a4ff463dfe4b490586e4920914ca1bc
oznchex <114240463+oznchex@users.noreply.github.com>
2024-09-24 15:01:59 +0900

    [feat]: 마이페이지 프로젝트 리스트 구현 - 기획 중인 프로젝트 - 진행 중인 프로젝트 - 종료된 프로젝트

--- src/app/mypage/project/page.tsx
+++ src/app/mypage/project/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import Footer from '@/components/Layout/Footer'
 import MyPageProjectList from '@/components/MyPage/MyPageProjectList'
 
 export default function MyPageProjectPage() {
     return (
         <div className="flex w-full flex-col justify-center items-center pt-[70px]">
             <MyPageProjectList />
+            <Footer />
         </div>
     )
 }
--- src/components/MyPage/MyPageProjectList.tsx
+++ src/components/MyPage/MyPageProjectList.tsx
@@ -46,12 +46,15 @@ export default function MyPageProjectList() {
 
     // completedStatusType이 "NOT_COMPLETED"인 프로젝트만 필터링
     const notCompletedProjects = myProjects.filter(project => project.completedStatusType === 'NOT_COMPLETED');
+    const completedProejcts = myProjects.filter(project => project.completedStatusType === 'COMPLETED');
+    const closedProjects = myProjects.filter(project => project.completedStatusType === 'CLOSED');
 
     // '더보기' 버튼을 누르지 않았을 때는 최대 3개의 프로젝트만 보여줌
-    const displayedProjects = showAll ? notCompletedProjects : notCompletedProjects.slice(0, 3);
-
+    const displayedNotCompletedProjects = showAll ? notCompletedProjects : notCompletedProjects.slice(0, 3);
+    const displayedCompletedProjects = showAll ? completedProejcts : completedProejcts.slice(0, 3);
+    const displayedClosedProjects = showAll ? closedProjects : closedProjects.slice(0, 3);
     return (
-        <div className="flex w-[56.25rem] flex-col items-start gap-[5rem] pt-[5.91rem]">
+        <div className="flex w-[56.25rem] flex-col items-start gap-[5rem] pt-[2.91rem]">
             {/* 기획 중인 프로젝트 */}
             <div className="flex flex-col w-[56.25rem]">
                 <div className="flex justify-between items-center self-stretch mb-[1.88rem]">
@@ -68,7 +71,55 @@ export default function MyPageProjectList() {
                 </div>
                 {/* 프로젝트를 3개씩 한 줄에 표시하고 더보기를 눌렀을 때 아래로 확장 */}
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-[0.75rem]">
-                    {displayedProjects.map((project) => (
+                    {displayedNotCompletedProjects.map((project) => (
+                        <Link key={project.projectId} href={`/project/${project.projectId}`}>
+                            <MyPageProjectWindow key={project.projectId} project={project} />
+                        </Link>
+                    ))}
+                </div>
+            </div>
+
+            {/* 진행 중인 프로젝트 */}
+            <div className="flex flex-col w-[56.25rem]">
+                <div className="flex justify-between items-center self-stretch mb-[1.88rem]">
+                    <div className="text-black text-[2rem] font-semibold leading-[3rem]" style={{ fontFamily: 'Pretendard, sans-serif' }}>
+                        진행 중인 프로젝트
+                    </div>
+                    {/* 더보기 버튼 */}
+                    <div
+                        className="text-[#6A6F7A] text-[1rem] font-medium leading-[1.5rem] cursor-pointer"
+                        onClick={() => setShowAll(prev => !prev)}  // '더보기' 클릭 시 상태 변경
+                    >
+                        {showAll ? '접기' : '더보기'}
+                    </div>
+                </div>
+                {/* 프로젝트를 3개씩 한 줄에 표시하고 더보기를 눌렀을 때 아래로 확장 */}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-[0.75rem]">
+                    {displayedCompletedProjects.map((project) => (
+                        <Link key={project.projectId} href={`/project/${project.projectId}`}>
+                            <MyPageProjectWindow key={project.projectId} project={project} />
+                        </Link>
+                    ))}
+                </div>
+            </div>
+
+            {/* 종료된 프로젝트 */}
+            <div className="flex flex-col w-[56.25rem]">
+                <div className="flex justify-between items-center self-stretch mb-[1.88rem]">
+                    <div className="text-black text-[2rem] font-semibold leading-[3rem]" style={{ fontFamily: 'Pretendard, sans-serif' }}>
+                        종료된 프로젝트
+                    </div>
+                    {/* 더보기 버튼 */}
+                    <div
+                        className="text-[#6A6F7A] text-[1rem] font-medium leading-[1.5rem] cursor-pointer"
+                        onClick={() => setShowAll(prev => !prev)}  // '더보기' 클릭 시 상태 변경
+                    >
+                        {showAll ? '접기' : '더보기'}
+                    </div>
+                </div>
+                {/* 프로젝트를 3개씩 한 줄에 표시하고 더보기를 눌렀을 때 아래로 확장 */}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-[0.75rem]">
+                    {displayedClosedProjects.map((project) => (
                         <Link key={project.projectId} href={`/project/${project.projectId}`}>
                             <MyPageProjectWindow key={project.projectId} project={project} />
                         </Link>
