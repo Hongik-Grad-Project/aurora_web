cae19ebe2232a1eb7a7229b4fcfece51f28839e2
oznchex <114240463+oznchex@users.noreply.github.com>
2024-11-27 01:01:24 +0900

    fix: charset 설정

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -26,7 +26,7 @@ export const RefreshAccessToken = async (accessToken: string) => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_AURORA_SERVER_URL}/token`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8',
       Authorization: `Bearer ${accessToken}`,
     },
     credentials: 'include',
